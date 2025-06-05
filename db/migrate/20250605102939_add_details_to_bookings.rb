38ff0a69137efd233c77dbdcf99d4b4a729560d8
Rafael Coorey <rafi.coorey@gmail.com>
2025-06-05 20:36:38 +1000

    updated details for bookings table

---
+++ db/migrate/20250605102939_add_details_to_bookings.rb
@@ -0,0 +1,8 @@
+class AddDetailsToBookings < ActiveRecord::Migration[7.1]
+  def change
+    add_column :bookings, :booking_status, :string
+    add_column :bookings, :number_of_guests, :integer
+    add_reference :bookings, :properties, null: false, foreign_key: true
+    add_reference :bookings, :user, null: false, foreign_key: true
+  end
+end
