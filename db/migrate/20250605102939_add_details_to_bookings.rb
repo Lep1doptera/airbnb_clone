53707e57fa1796a3bc8555771a39f5c1f7207bc4
Rafael Coorey <rafi.coorey@gmail.com>
2025-06-05 21:11:24 +1000

    database created

--- db/migrate/20250605102939_add_details_to_bookings.rb
+++ db/migrate/20250605102939_add_details_to_bookings.rb
@@ -3,6 +3,6 @@ class AddDetailsToBookings < ActiveRecord::Migration[7.1]
     add_column :bookings, :booking_status, :string
     add_column :bookings, :number_of_guests, :integer
     add_reference :bookings, :properties, null: false, foreign_key: true
-    add_reference :bookings, :user, null: false, foreign_key: true
+    add_reference :bookings, :users, null: false, foreign_key: true
   end
 end
