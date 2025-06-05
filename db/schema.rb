ea851c643c3eaf52e44d9ed75d89854d6af13e4b
Maksym Kozin <maksymkozintest@gmail.com>
2025-06-05 20:27:06 +1000

    unfinished yet

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,18 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_05_091506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "properties", force: :cascade do |t|
+    t.string "property_address"
+    t.integer "number_of_guests"
+    t.text "description"
+    t.integer "price_per_night"
+    t.string "property_image_url"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,4 +38,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_05_091506) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "properties", "users"
 end
