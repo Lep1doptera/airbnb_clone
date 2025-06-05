ac34f690f9ac606d9230b3d3ea203a4088707ff6
Rafael Coorey <rafi.coorey@gmail.com>
2025-06-05 21:21:51 +1000

    added number of guests validations to bookings model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   validates :check_in_date, :check_out_date, presence: true
+  validates :number_of_guests, numericality: { only_integer: true, greater_than: 0 }
 end
