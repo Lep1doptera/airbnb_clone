ec39bc55cdfb66994f0cb200d592542697bf5a3e
Amanda Ferguson <ferguson.am19@gmail.com>
2025-06-06 21:48:15 +1000

    Created Seed File

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
-  belongs_to :properties
-  belongs_to :user
-
+  belongs_to :property, foreign_key: "properties_id"
+  belongs_to :user, foreign_key: "users_id"
+  
   validates :check_in_date, :check_out_date, presence: true
   validates :number_of_guests, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :properties, foreign_key: "users_id"
+  has_many :bookings, foreign_key: "users_id"
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
