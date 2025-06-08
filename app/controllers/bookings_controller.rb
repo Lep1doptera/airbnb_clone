f2a7a5ef17aec8301fe778eb3bf33f8b282f6f62
Rafael Coorey <rafi.coorey@gmail.com>
2025-06-08 14:58:43 +1000

    bookings controller completed

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,58 @@
+class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to @booking
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(
+      :check_in_date,
+      :check_out_date,
+      :number_of_guests,
+      :booking_status,
+      :property_id
+    )
+  end
+end
