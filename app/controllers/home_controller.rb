34682fdf5471694e63e2cbfb12e769ebc3c4d1b4
Maksym Kozin <maksymkozintest@gmail.com>
2025-06-10 20:41:52 +1000

    added random 4 properties display, cliable and logo to go to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @bookings = Booking.all
     @properties = Property.all
+    @random_properties = Property.order("RANDOM()").limit(4)
   end
 end
