6b90681f50d450a6b0e0b414633f380e275f33e1
Amanda Ferguson <ferguson.am19@gmail.com>
2025-06-12 21:20:56 +1000

    Adding partials to user dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   def index
-    @bookings = Booking.all
-    @properties = Property.all
     @random_properties = Property.order("RANDOM()").limit(4)
+
+    if user_signed_in?
+      @user_bookings = current_user.bookings
+      @user_properties = current_user.properties.includes(:bookings)
+    end
+
   end
 end
--- db/migrate/20250610094008_rename_users_id_to_user_id_in_properties.rb
+++
@@ -1,5 +0,0 @@
-class RenameUsersIdToUserIdInProperties < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :properties, :users_id, :user_id
-  end
-end
