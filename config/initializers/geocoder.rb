42051780b0a2a0d10ab1f9c1827c676b0ca63f70
Amanda Ferguson <ferguson.am19@gmail.com>
2025-06-17 19:29:31 +1000

    Changed secrets management from rails creds to .env

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,7 +3,7 @@ Geocoder.configure(
   # [...]
   timeout: 5, # seconds
   lookup: :mapbox, # Defaults to :nominatim, which is less reliable but free
-  api_key: Rails.application.credentials.dig(:mapbox, :api_key),
+  api_key: ENV["MAPBOX_API_KEY"],
   units: :km, # Defaults to miles (:mi)
   # [...]
 )
